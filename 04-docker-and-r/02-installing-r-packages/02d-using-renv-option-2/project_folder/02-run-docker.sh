08c6101bde186a8d826317df69194cedec8a7fd6
Rahul Sangole <rsangole@apple.com>
2022-06-18 12:40:28 -0700

    Finishes 02d

--- 04-docker-and-r/02-installing-r-packages/02d-using-renv-option-2/project_folder/02-run-docker.sh
+++
@@ -1,9 +0,0 @@
-YOUR_LOCAL_RENV_CACHE_PATH="/Users/rahul/github/github.com/user2022-r-for-docker/04-docker-and-r/02d-using-renv-option-2/renv-cache"
-YOUR_LOCAL_PROJECT_FOLDER="/Users/rahul/github/github.com/user2022-r-for-docker/04-docker-and-r/02d-using-renv-option-2/project_folder"
-docker run \
-	--rm \
-	--volume $YOUR_LOCAL_RENV_CACHE_PATH:/renv/cache \
-	--volume $YOUR_LOCAL_PROJECT_FOLDER:/home/work \
-	-it \
-	docker_for_r/package_example_renv_2:v1.0 \
-	bash
---
+++ 04-docker-and-r/02-installing-r-packages/02d-using-renv-option-2/project_folder/script.R
@@ -0,0 +1,5 @@
+library(data.table)
+
+dat <- as.data.table(mtcars)
+
+dat[]
